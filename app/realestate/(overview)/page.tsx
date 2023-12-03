68ff21bf635b8c46435cca41ee6c826d5fc04c32
Mathys Taljaard <mathysjtaljaard@users.noreply.github.com>
2023-12-03 14:32:43 -0500

    Add Material UI

--- app/realestate/(overview)/page.tsx
+++ app/realestate/(overview)/page.tsx
@@ -1,5 +1,16 @@
+import { Container, Grid } from "@mui/material";
+
 export default function Page() {
-    return (
-        <h1>Estates</h1>
-    )
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <h1>Real Estate Dashboard</h1>
+        </Grid>
+        <Grid item xs={4}>
+          <h2>Things to be done</h2>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
